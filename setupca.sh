d0a5d60ed951956ff205be4c6bfa548e637ea3d7
Johan Swensson <kupo@kupo.se>
2016-04-17 16:06:55 +0200

    change pem to crt

--- setupca.sh
+++ setupca.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 openssl genrsa -out rootCA.key 4096 -des3
-openssl req -x509 -new -nodes -key rootCA.key -days 1024 -out rootCA.pem
+openssl req -x509 -new -nodes -key rootCA.key -days 1024 -out rootCA.crt
