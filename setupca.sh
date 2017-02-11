f5ba3689262116fa690687cb73b67dc099073126
Johan Swensson <kupo@kupo.se>
2017-02-11 21:08:43 +0100

    refactor CA

--- setupca.sh
+++ setupca.sh
@@ -1,4 +1,51 @@
 #!/bin/bash
 
-openssl genrsa -out rootCA.key 4096 -des3
-openssl req -x509 -new -nodes -key rootCA.key -days 1024 -out rootCA.crt
+mkdir -p ca
+pushd ca
+#init root ca
+ln -s ../root.cnf openssl.cnf
+mkdir certs crl newcerts private
+chmod 700 private
+touch index.txt
+echo 1000 > serial
+
+openssl genrsa -aes256 -out private/ca.key 4096
+
+chmod 400 private/ca.key
+
+openssl req -config openssl.cnf \
+        -key private/ca.key.pem \
+        -new -x509 -days 365 -sha256 -extensions v3_ca
+        -out certs/ca.crt
+
+openssl x509 -noout -text -in certs/ca.crt
+
+#intermediate
+mkdir -p intermediate
+pushd intermediate
+ln -s ../../intermediate.cnf openssl.cnf
+
+mkdir certs crl csr newcerts private
+echo 1000 > crlnumber
+
+openssl genrsa -aes256 \
+        -out private/intermediate.key.pem 4096
+
+chmod 400 private/intermediate.key.pem
+
+openssl req -config openssl.cnf -new -sha256 \
+      -key private/intermediate.key \
+      -out csr/intermediate.csr
+
+popd
+
+openssl ca -config openssl.cnf -extensions v3_intermediate_ca \
+      -days 180 -notext -md sha256 \
+      -in intermediate/csr/intermediate.csr \
+      -out intermediate/certs/intermediate.crt
+
+chmod 444 intermediate/certs/intermediate.crt
+
+#create ca-chain file to distribute
+
+cat intermediate/certs/intermediate.crt certs/ca.crt > intermediate/certs/ca-chain.crt
